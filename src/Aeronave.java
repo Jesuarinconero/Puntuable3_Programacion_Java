349abf8128bfa3dcd9113b608c952356ce9a65bb
Jesuarinconero <jvergaragonzalez@safareyes.es>
2023-01-30 09:53:05 +0100

    Hacer constructor

---
+++ src/Aeronave.java
@@ -0,0 +1,22 @@
+public class Aeronave {
+    private String codigo;
+    private String fechaentrega;
+    private String L2GA;
+    private int horastrabajo;
+    private int horastrabajoempleada;
+    private double coste;
+    private  double precioventa;
+   public Aeronave(String cod, String fechaE){
+
+    }
+    public String getFechaentrega() {
+        return fechaentrega;
+    }
+    public int horasbenif(){
+        return horastrabajo-horastrabajoempleada
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/avionetas.java
@@ -0,0 +1,2 @@
+public class avionetas {
+}
